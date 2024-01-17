e098da81a8a6bcc872a6c872aacf2da678eb6f26
MatheusKpv <matheusmatias1910@gmail.com>
2024-01-16 22:37:12 -0300

    codigo final

--- jogo/app.js
+++ jogo/app.js
@@ -4,7 +4,6 @@ let numeroSecreto = gerarNumeroSecreto();
 let numeroTentativas = 1;
 msgInicial();
 
-
 function verificarChute() {
     let chute = document.querySelector('input').value;
     let tentativa = numeroTentativas > 1 ? 'tentativas' : 'tentativa';
